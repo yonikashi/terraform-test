ce3cd6d71e2bac9d1c946ee7b3befe9e74774408
test-name <test@email.com>
2019-07-21 16:53:58 +0300

    tt

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@
 
 provider "aws" {
   region                  = "${var.aws_region}"
-  shared_credentials_file = "/credentials"
+  shared_credentials_file = "/var/jenkins_home/workspace/Test-Job/terraform-test/credentials"
   profile                 = "default"
 }
 
