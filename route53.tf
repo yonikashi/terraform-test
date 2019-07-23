41ff0827e07a1b27766cb5130598d58b8da9ae83
test-name <test@email.com>
2019-07-23 17:01:04 +0300

    horizon6

--- route53.tf
+++ route53.tf
@@ -60,39 +60,49 @@ resource "aws_route53_record" "ip-test-core-5" {
 resource "aws_route53_record" "test-core-1" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "core-test-1"
-  type    = "A"
-  ttl     = "300"
-  records = ["${aws_lb.node1-nlb.dns_name}"]
+  type    = "A" 
+ alias {
+    name                   = "${aws_lb.node1-nlb.dns_name}"
+    zone_id                = "${aws_lb.node1-nlb.zone_id}"
+  }
 }
 
 resource "aws_route53_record" "test-core-2" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "core-test-2"
   type    = "A"
-  ttl     = "300"
-  records = ["${aws_lb.node2-nlb.dns_name}"]
+  alias {
+    name                   = "${aws_lb.node2-nlb.dns_name}"
+    zone_id                = "${aws_lb.node2-nlb.zone_id}"
+  }
 }
 
 resource "aws_route53_record" "test-core-3" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "core-test-3"
   type    = "A"
-  ttl     = "300"
-  records = ["${aws_lb.node3-nlb.dns_name}"]
+ alias {
+    name                   = "${aws_lb.node3-nlb.dns_name}"
+    zone_id                = "${aws_lb.node3-nlb.zone_id}"
+  }
 }
 
 resource "aws_route53_record" "test-core-4" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "core-test-4"
   type    = "A"
-  ttl     = "300"
-  records = ["${aws_lb.node4-nlb.dns_name}"]
+ alias {
+    name                   = "${aws_lb.node4-nlb.dns_name}"
+    zone_id                = "${aws_lb.node4-nlb.zone_id}"
+  }
 }
 
 resource "aws_route53_record" "test-core-5" {
   zone_id = "${aws_route53_zone.primary.zone_id}"
   name    = "core-test-5"
   type    = "A"
-  ttl     = "300"
-  records = ["${aws_lb.node5-nlb.dns_name}"]
+ alias {
+    name                   = "${aws_lb.node5-nlb.dns_name}"
+    zone_id                = "${aws_lb.node5-nlb.zone_id}"
+  }
 }
