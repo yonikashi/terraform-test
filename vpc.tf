238e90e20a4330b96f61933839b250ef19d7f420
test-name <test@email.com>
2019-07-25 16:25:29 +0300

    (peering)

--- vpc.tf
+++ vpc.tf
@@ -26,11 +26,10 @@ resource "aws_internet_gateway" "gw" {
 resource "aws_route" "peering-mgmt-to-tests" {
   vpc_id = "vpc-080d4cf2e782cf760"
   route_table_id = "rtb-0e0cadd5d7d99912c"
-
-  route {
+#  route {
     cidr_block = "172.31.0.0/16"
     gateway_id = "${aws_vpc_peering_connection.jenkinstocore.id}"
-  }
+#  }
 }
 
 # Define the public route table
