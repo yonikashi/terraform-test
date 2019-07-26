fc41d3da3f1e55c381514da2d3a1737fe6021253
test-name <test@email.com>
2019-07-26 04:36:09 +0300

    (Fixed-output)

--- output.tf
+++ output.tf
@@ -3,7 +3,13 @@ output "aws_eip_natip" {
   description = "The NAT IP address of the Stellar Subnet for Whitelisting"
 }
 
+output "aws_test_client_ip" {
+  value = "${aws_instance.test-load-client-1.private_ip}"
+  description = "The Private IP address of Test-Client"
+}
 
 
-
-
+output "aws_prom_dns" {
+  value = "${aws_alb.prometheus-nlb.public_dns}"
+  description = "The address of Prometheus NLB"
+}
